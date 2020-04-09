9d75eea3d6afbb4e8ec45fe7ed92ae64c0c2cf2b
namnguyentat <nguyentatnam53ac@gmail.com>
2020-04-09 09:37:20 +0700

    Add dns sd

--- renderer.js
+++ renderer.js
@@ -4,3 +4,42 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+const bonjour = require('bonjour')();
+let services = [];
+
+window.addEventListener('DOMContentLoaded', () => {
+  document
+    .getElementById('search-button')
+    .addEventListener('click', findService);
+
+  function showResult() {
+    const result = document.getElementById('result');
+    let html = '';
+    services.forEach((service, index) => {
+      html += `
+        <tr>
+          <td>${index + 1}</td>
+          <td>${JSON.stringify(service)}</td>
+        </tr>
+      `;
+    });
+    if (result) {
+      result.innerHTML = html;
+    }
+  }
+
+  function findService() {
+    services = [];
+    // Find all koov services
+    const options = document.getElementById('search-input').value;
+    if (options.trim().length === 0) {
+      console.log('Please input search options');
+      return;
+    }
+    bonjour.find(JSON.parse(options), function (service) {
+      services.push(service);
+      showResult();
+      console.log('Found service:', service);
+    });
+  }
+});
